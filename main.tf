d6e351fa97802f27f536747131386641c0f4c70e
Thomas Cate <tcate@chef.io>
2017-05-12 16:40:38 -0500

    moved example to main

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 resource "aws_instance" "chefserver" {
   count = "${var.studentCount}"
   ami           = "${var.ami}"
-  instance_type = "t2.micro"
+  instance_type = "${var.chefServerInstanceType}"
   tags {
     Name = "${element(var.cardTable, count.index)}.chefserver.e9.io",
     X-Contact = "tcate@chef.io",
@@ -25,7 +25,7 @@ resource "aws_eip" "chefserverip" {
 resource "aws_instance" "automate" {
   count = "${var.studentCount}"
   ami           = "${var.ami}"
-  instance_type = "t2.micro"
+  instance_type = "${var.automateInstanceType}"
   tags {
     Name = "${element(var.cardTable, count.index)}.automate.e9.io",
     X-Contact = "tcate@chef.io",
@@ -42,7 +42,7 @@ resource "aws_eip" "automateip" {
 resource "aws_instance" "runner" {
   count = "${var.studentCount}"
   ami           = "${var.ami}"
-  instance_type = "t2.micro"
+  instance_type = "${var.runnerInstanceType}"
   tags {
     Name = "${element(var.cardTable, count.index)}.runner.e9.io",
     X-Contact = "tcate@chef.io",
@@ -59,7 +59,7 @@ resource "aws_eip" "runnerip" {
 resource "aws_instance" "prod" {
   count = "${var.studentCount}"
   ami           = "${var.ami}"
-  instance_type = "t2.micro"
+  instance_type = "${var.prodInstanceType}"
   tags {
     Name = "${element(var.cardTable, count.index)}.prod.e9.io",
     X-Contact = "tcate@chef.io",
